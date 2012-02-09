055f0014d6fe3cca8c1c2d8647675570f999f9fe
Naitik Shah <n@daaku.org>
2012-02-09 13:38:26 -0800

    travis-ci, mocha for tests

--- test/tmpl-test.js
+++ test/tmpl-test.js
@@ -1,7 +1,7 @@
 var tmpl = require('tmpl')
   , assert = require('assert')
 
-exports['basic name substitution'] = function(beforeExit) {
+exports['basic name substitution'] = function() {
   assert.equal(
     tmpl('the answer is {answer}', { answer: 42 }),
     'the answer is 42')
